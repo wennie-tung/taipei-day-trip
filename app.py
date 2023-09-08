ee3bd44d2f300c96bf310749eacf69909a3e45dc
wennie-tung <wennie1517@gmail.com>
2023-09-09 02:36:07 +0800

    add api functions

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import mysql.connector
 # 連接到本機的 MySQL
 mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
+    user="wennie",
     password="password",
     database="websiteTT"
 )
@@ -55,7 +55,7 @@ def getAttraction():
 	mycursor.execute(sql, val)
 	searchResult = mycursor.fetchall()
 	# print(searchResult)
-	print(len(searchResult))
+	# print(len(searchResult))
 
 	for result in searchResult:
 		result['images'] = json.loads(result['images'])
