3d1a64541ce6c83b9b3026154de88d36ed79d9dd
wennie-tung <wennie1517@gmail.com>
2023-09-22 18:30:46 +0800

    add week 4 assignment

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, jsonify, request, json
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+import mysql.connector
 app = Flask(__name__, static_folder="public", static_url_path="/")
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
-
-# MySQL
-import mysql.connector
+app.config['JWT_SECRET_KEY'] = 'wehelp-taipei-day-trip'
+# Token 時效性 7 天
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 604800
+jwt = JWTManager(app)
 
 # 連接到本機的 MySQL
 mydb = mysql.connector.connect(
@@ -13,7 +16,7 @@ mydb = mysql.connector.connect(
     password="password",
     database="websiteTT"
 )
-# mycursor = mydb.cursor()
+mycursor = mydb.cursor()
 
 # Pages
 @app.route("/")
@@ -111,5 +114,80 @@ def getMrt():
 	except Exception as e:
 		return jsonify({"error": True, "message": "伺服器內部錯誤"}), 500
 
+# 註冊 API
+@app.route("/api/signup", methods=['POST'])
+def signUp():
+	name = request.json.get('name')
+	account = request.json.get('account')
+	password = request.json.get('password')
+	sql = 'SELECT * FROM member WHERE account = %s'
+	mycursor.execute(sql, (account,))
+	existing_account = mycursor.fetchall()
 
-app.run(host="0.0.0.0", port=3000)
+	if existing_account:
+		data = {'message': '註冊失敗，請再試一次'}
+		return jsonify(data), 401
+	else:
+		sql = 'INSERT INTO member(name, account, password) VALUES(%s, %s, %s)'
+		val = (name, account, password)
+		mycursor.execute(sql, val)
+		mydb.commit()
+		data = {'message': '註冊成功，請重新登入'}
+		return jsonify(data)
+
+# 登入 API
+@app.route("/api/signin", methods=['POST'])
+def signIn():
+	account = request.json.get('account')
+	password = request.json.get('password')
+	sql = 'SELECT * FROM member WHERE account = %s and password = %s'
+	mycursor.execute(sql, (account,password))
+	user_data = mycursor.fetchone()
+	print(user_data)
+
+	if user_data:
+		id, name, email, password = user_data
+		user_identity = {
+			"id" : id,
+			"name" : name,
+			"account" : email,
+		}
+		access_token = create_access_token(identity = user_identity)
+		data = {
+			"message": "登入成功！網頁重新載入中...",
+			"access_token" : access_token
+			}
+		return jsonify(data)
+	else:
+		data = {'message': '登入失敗'}
+		return jsonify(data), 401
+	
+@app.route('/api/verifyToken', methods=['GET'])
+@jwt_required()
+def verifyToken():
+	try:
+		# 取得 token 內的資料
+		current_user = get_jwt_identity()
+		print(current_user)
+
+		if current_user:
+			# 從用戶資訊回傳給前端
+			user_id = current_user['id']
+			user_name = current_user['name']
+			user_account = current_user['account']
+
+			response_data = {
+				'id' : user_id,
+				'name' : user_name,
+				'account' : user_account,
+			}
+			return jsonify(response_data), 200
+		
+		else:
+			return jsonify(message = "Token 驗證失效，請重新登入"), 401
+		
+	except Exception as e:
+		return jsonify(message='"error'), 401
+	
+
+app.run(host="0.0.0.0", debug=True, port=3000)
